0f4a867b77690c376e687b9f3f49bfe93fa656fc
Rhys Paulino <rhys.paulino@vanarts.com>
2023-08-18 13:16:47 -0700

    pender app

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  // pets:any[] = []
+
+
+
+  constructor(private http:HttpClient){}
+ 
+  // ngOnInit(): void{
+  //   this.http.get<any>('https://cms.alexsumina.me/api/pender-app-screen?populate=*').subscribe(response=>{
+  //     this.pets = response.data
+  //     console.log(this.pets)
+  //   })
+
+  // }
+
+
+}
